d5cf05f6a0d539b1d90ded9d0373bf54cccdd2f0
SoWieMarkus <wie.markus@web.de>
2025-06-21 12:04:56 +0200

    Added raw body parser for twitch verification

--- packages/backend/src/routes/twitch-webhook.ts
+++ packages/backend/src/routes/twitch-webhook.ts
@@ -4,6 +4,15 @@ import { verifyTwitchSignature } from "../middlewares/twitch-verification";
 
 const router = express.Router();
 
-router.post("/webhook/stream", verifyTwitchSignature, TwitchWebhookController.streamStateWebhook);
+router.post(
+	"/webhook/stream",
+	// the raw body parser is needed for signature verification
+	express.raw({
+		type: "application/json",
+	}),
+	verifyTwitchSignature,
+	express.json(),
+	TwitchWebhookController.streamStateWebhook,
+);
 
 export default router;
