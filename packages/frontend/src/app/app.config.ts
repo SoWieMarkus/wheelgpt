25f0e73e5fbc2ecc0047d03acedcd9d413aba0b5
SoWieMarkus <wie.markus@web.de>
2025-07-29 11:32:32 +0200

    Fixed linter issues

--- packages/frontend/src/app/app.config.ts
+++ packages/frontend/src/app/app.config.ts
@@ -1,12 +1,11 @@
-import { HttpClient, provideHttpClient, withInterceptors } from "@angular/common/http";
+import { provideHttpClient, withInterceptors } from "@angular/common/http";
 import { type ApplicationConfig, provideZoneChangeDetection } from "@angular/core";
 import { provideRouter } from "@angular/router";
 import { provideTranslateService } from "@ngx-translate/core";
-import { provideTranslateHttpLoader } from '@ngx-translate/http-loader';
+import { provideTranslateHttpLoader } from "@ngx-translate/http-loader";
 import { routes } from "./app.routes";
 import { authenticationInterceptor } from "./http/authentication.interceptor";
 import { unauthorizedInterceptor } from "./http/unauthorized.interceptor";
-
 
 export const appConfig: ApplicationConfig = {
 	providers: [
@@ -16,8 +15,8 @@ export const appConfig: ApplicationConfig = {
 		provideTranslateService({
 			loader: provideTranslateHttpLoader({
 				prefix: "./locale/",
-				suffix: ".json"
-			})
-		})
+				suffix: ".json",
+			}),
+		}),
 	],
 };
