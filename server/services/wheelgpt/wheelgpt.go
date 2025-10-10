2194decb6a3f044696fe1c513fa3506245a3d063
SoWieMarkus <wie.markus@web.de>
2025-10-10 20:43:57 +0200

    Add NewInstance function to create WheelGPT instances

--- server/services/wheelgpt/wheelgpt.go
+++ server/services/wheelgpt/wheelgpt.go
@@ -10,6 +10,14 @@ type WheelGPT struct {
 	DB db.Database
 }
 
+func NewInstance(db db.Database) WheelGPT {
+	return WheelGPT{DB: db}
+}
+
 func (bot *WheelGPT) Initialize() error {
+	// Load all channels from the database
+	// Update the details of all channels
+	// Update the current live status of all channels
+	// Join all the channels
 	return nil
 }
