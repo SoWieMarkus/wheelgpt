e2e4b741069afd7f6162ed528751efa17d318b43
SoWieMarkus <wie.markus@web.de>
2025-10-10 20:44:29 +0200

    Refactor main function to load database configuration and initialize WheelGPT instance

--- server/cmd/wheelgpt/main.go
+++ server/cmd/wheelgpt/main.go
@@ -1,7 +1,30 @@
 package main
 
-import "log/slog"
+import (
+	"context"
+	"log/slog"
+
+	"github.com/SoWieMarkus/wheelgpt/core/config"
+	"github.com/SoWieMarkus/wheelgpt/core/db"
+	"github.com/SoWieMarkus/wheelgpt/services/wheelgpt"
+)
 
 func main() {
 	slog.Info("WheelGPT started")
+
+	config, err := config.LoadDatabaseConfig()
+	if err != nil {
+		panic("Failed to load database config: " + err.Error())
+	}
+
+	ctx := context.Background()
+
+	postgres := db.NewPostgresDB(ctx, config)
+	defer postgres.Close()
+
+	bot := wheelgpt.NewInstance(postgres)
+	bot.Initialize()
+
+	// TODO start the bot
+
 }
